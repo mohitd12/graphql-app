6bdc1bf9ae5ce979dd556374bf5868f6af9b6271
Mohit Das <mohitdass589@gmail.com>
2021-01-02 21:46:14 +0530

    create express graphiql server

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+
+// schema
+const schema = require('./schema');
 
 const app = express();
 const PORT = 5000;
+
+// graphQL server
+app.use(
+	'/graphql',
+	graphqlHTTP({
+		schema: schema,
+		graphiql: true
+	})
+);
 
 app.listen(PORT, function() {
 	console.log('Server is listening on port', PORT);
